cae5de42142f32ac55a78832f7accbd56de5526a
VolodymyrSorokin <depsorokin@ukr.net>
2023-12-27 22:10:48 +0100

    Query10

--- Program.cs
+++ Program.cs
@@ -267,13 +267,19 @@ namespace Practice_Linq
         {
             //Query 10: Вивести з 5-го по 10-тий (включно) матчі Gold Cup, які відбулися у липні 2023 р.
 
-            var selectedGames = games;    // Корегуємо запит !!!
+            var selectedGames = games
+            .Where(game => game.Tournament == "Gold Cup" && game.Date.Month == 7 && game.Date.Year == 2023)
+            .Skip(4)
+            .Take(6)
+            .ToList();
+            // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 10 ========================");
-
-            // див. приклад як має бути виведено:
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
         }
 
